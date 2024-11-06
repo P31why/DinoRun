792f0ca4a4fafdd4fa85fd816981752c8e90e1b3
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-06 22:30:19 +0300

    added Awake method for complete menuse loading

--- Assets/scripts/UI/MenuManager.cs
+++ Assets/scripts/UI/MenuManager.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _storeMenu;
+    private void Awake()
+    {
+        _mainMenu.SetActive(true);
+        _storeMenu.SetActive(false);
+    }
     public void GameStart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
