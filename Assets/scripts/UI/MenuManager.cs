79b2b5c53fbcce5f87abcd769bcc4b2fb458433e
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-06 21:46:13 +0300

    added GameStart function

--- Assets/scripts/UI/MenuManager.cs
+++ Assets/scripts/UI/MenuManager.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GameStart()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
