a2eb9a779397f2c3850755f449b04ec12f502999
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-07 20:23:19 +0300

    added new menus gameObjects

--- Assets/scripts/UI/MenuManager.cs
+++ Assets/scripts/UI/MenuManager.cs
@@ -5,10 +5,14 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _storeMenu;
+    [SerializeField] private GameObject _storeSkins;
+    [SerializeField] private GameObject _storeMusic;
     private void Awake()
     {
         _mainMenu.SetActive(true);
         _storeMenu.SetActive(false);
+        _storeSkins.SetActive(false);
+        _storeMusic.SetActive(false);
     }
     public void GameStart()
     {
