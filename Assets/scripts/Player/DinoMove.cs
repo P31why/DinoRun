c84b6c4c211a2df5ed6fe69b95976adf59a4836a
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-10 14:11:59 +0300

    added jumping fo player

--- Assets/scripts/Player/DinoMove.cs
+++ Assets/scripts/Player/DinoMove.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class DinoMove : MonoBehaviour
 {
-
+    [SerializeField]private int _jumpf = DinoStats.Instance.JumpForce;
+    [SerializeField]private Rigidbody2D _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("jump");
+            _rb.AddForce(new Vector2(0, _jumpf), ForceMode2D.Impulse);
+        }
+    }
+    private void FixedUpdate()
+    {
+       
+    }
 }
