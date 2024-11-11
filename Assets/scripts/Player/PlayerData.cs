ef1984b0b75202010e70e63ee1a8a01f6b9ef88d
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-11 14:55:05 +0300

    Added class for serialization player data

---
+++ Assets/scripts/Player/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.scripts.Player
+{
+    public class PlayerData
+    {
+        private int _score;
+        private int _money;
+        public PlayerData(int _score,int _money)
+        {
+            this._score = _score;
+            this._money = _money;
+        }
+        public void SaveData()
+        {
+
+        }
+    }
+}
