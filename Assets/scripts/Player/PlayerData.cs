85d13e6669341dfc62cbf25659178392d89545a7
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-11 20:16:51 +0300

    added get & set for fields and remove SaveData function

--- Assets/scripts/Player/PlayerData.cs
+++ Assets/scripts/Player/PlayerData.cs
@@ -10,14 +10,21 @@ namespace Assets.scripts.Player
     {
         private int _score;
         private int _money;
+        public PlayerData() { }
         public PlayerData(int _score,int _money)
         {
             this._score = _score;
             this._money = _money;
         }
-        public void SaveData()
+        public int Score
         {
-
+            get => _score;
+            set => _score = value;
+        }
+        public int Money
+        {
+            get => _money;
+            set => _money = value;
         }
     }
 }
