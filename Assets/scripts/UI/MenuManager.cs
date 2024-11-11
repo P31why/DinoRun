3f659fb7ca4f0027cd89b952257a6e78fef429de
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-11 21:42:21 +0300

    added text with best score in main menu

--- Assets/scripts/UI/MenuManager.cs
+++ Assets/scripts/UI/MenuManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
@@ -7,12 +8,17 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject _storeMenu;
     [SerializeField] private GameObject _storeSkins;
     [SerializeField] private GameObject _storeMusic;
+    [SerializeField] private TMP_Text _bestTextScore;
     private void Awake()
     {
         _mainMenu.SetActive(true);
         _storeMenu.SetActive(false);
         _storeSkins.SetActive(false);
         _storeMusic.SetActive(false);
+    }
+    private void Start()
+    {
+        _bestTextScore.text = "Рекорд: "+DinoStats.Instance.Score.ToString();
     }
     public void GameStart()
     {
