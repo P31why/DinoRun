b7d4dab687eebb3a698f1b42902f64b5d3887e44
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-11 20:46:01 +0300

    fixed bug with ui

--- Assets/scripts/Player/DinoResults.cs
+++ Assets/scripts/Player/DinoResults.cs
@@ -6,9 +6,11 @@ public class DinoResults : MonoBehaviour
 {
     [SerializeField] private int _currentScore=0;
     [SerializeField] private TMP_Text _textScore;
+    [SerializeField] private GameObject _textobject;
     private void Start()
     {
         StartCoroutine(AddScore());
+        _textobject.SetActive(true);
         _textScore.text = "0";
     }
     private void ChangeScoreText()
