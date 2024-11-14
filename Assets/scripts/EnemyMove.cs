700646751cdeb50a63436cb539939d67fb4b56b5
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-14 13:52:55 +0300

    added code for moving and deleting enemy

--- Assets/scripts/EnemyMove.cs
+++ Assets/scripts/EnemyMove.cs
@@ -1,6 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 dir;
+    private void Awake()
+    {
+        StartCoroutine(DeleteThisObj());
+    }
+    private void Update()
+    {
+        transform.Translate(dir * speed*Time.deltaTime);
+    }
+    IEnumerator DeleteThisObj()
+    {
+        yield return new WaitForSeconds(15);
+        Destroy(gameObject);
+    }
 }
