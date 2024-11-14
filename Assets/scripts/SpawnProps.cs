d430edfb83e196772119f6428ee9fe226433b17b
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-14 13:09:50 +0300

    added corrutine for span objects

--- Assets/scripts/SpawnProps.cs
+++ Assets/scripts/SpawnProps.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class SpawnProps : MonoBehaviour
 {
-
+    [SerializeField] private GameObject[] structs;
+    private void Awake()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+    IEnumerator SpawnObjects()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            int obj_number=Random.Range(1, 10);
+            if(obj_number>0 && obj_number<=3)
+            {
+                Instantiate(structs[0], transform);
+            }
+            else if (obj_number >= 4 && obj_number <= 5)
+            {
+                Instantiate(structs[0], transform);
+            }
+            
+        }
+    }
 }
