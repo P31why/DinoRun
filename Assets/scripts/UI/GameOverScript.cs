0e7675b3bbea036b30979d43f719ac15405e3ede
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-16 18:02:10 +0300

    add hiding PlayerStats when player dead

--- Assets/scripts/UI/GameOverScript.cs
+++ Assets/scripts/UI/GameOverScript.cs
@@ -8,12 +8,14 @@ public class GameOverScript : MonoBehaviour
     [SerializeField] private TMP_Text _collectedMoneyText;
     [SerializeField] private DinoResults _dinoScore;
     [SerializeField] private CoinStats _coinScr;
+    [SerializeField] private GameObject _statsText;
     public void RestartGame()
     {
         SceneManager.LoadScene(sceneBuildIndex:1);
     }
     public void ShowStatistics()
     {
+        _statsText.SetActive(false);
         _scoreText.text = _dinoScore.CurrentScore.ToString();
         _collectedMoneyText.text = _coinScr.CurrentMoney.ToString();
         DinoStats.Instance.Money += _coinScr.CurrentMoney;
