cccc70051c93dc0d61c5e81595ddf5474a996780
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-11-17 21:53:29 +0300

    updated script

--- Assets/scripts/UI/StoreDino.cs
+++ Assets/scripts/UI/StoreDino.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class StoreDino : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _buyGreenText;
+    [SerializeField] private TMP_Text _aplGreenText;
     [SerializeField] private TMP_Text _buyRedText;
     [SerializeField] private TMP_Text _aplRedText;
     [SerializeField] private TMP_Text _buyBlueText;
@@ -12,45 +12,57 @@ public class StoreDino : MonoBehaviour
     [SerializeField] private TMP_Text _buyYellowText;
     [SerializeField] private TMP_Text _aplYellowText;
 
-    private void Start()
+    public void LoadStore()
     {
-        if (DinoStats.Instance.SetSkin == 0)
+        List<DinoSkin> buff = DinoStats.Instance.skinsUnlock;
+        foreach (DinoSkin i in buff)
         {
-            _buyGreenText.text = "Выбрано";
-        }
-        else
-        {
-            List<DinoSkin> buff = DinoStats.Instance.skinsUnlock;
-            foreach (DinoSkin i in buff)
+            if (i.skinId == 1 && i.acquired)
             {
-                if(i.skinId == 1 && i.acquired)
-                {
-                    _buyRedText.text = "Куплено";
-                }
-                else
-                {
-                    _buyRedText.text = "Купить за 10$";
-                }
+                _buyRedText.text = "Куплено";
+            }
+            else if (i.skinId == 1 && !i.acquired)
+            {
+                _buyRedText.text = "Купить за 10$";
+            }
 
-                if (i.skinId == 2 && i.acquired)
-                {
-                    _buyBlueText.text = "Куплено";
-                }
-                else
-                {
-                    _buyBlueText.text = "Купить за 40$";
-                }
+            if (i.skinId == 2 && i.acquired)
+            {
+                _buyBlueText.text = "Куплено";
+            }
+            else if (i.skinId == 1 && !i.acquired)
+            {
+                _buyBlueText.text = "Купить за 40$";
+            }
 
-                if (i.skinId == 3 && i.acquired)
-                {
-                    _buyYellowText.text = "Куплено";
-                }
-                else
-                {
-                    _buyYellowText.text = "Купить за 30$";
-                }
+            if (i.skinId == 3 && i.acquired)
+            {
+                _buyYellowText.text = "Куплено";
+            }
+            else if (i.skinId == 1 && !i.acquired)
+            {
+                _buyYellowText.text = "Купить за 30$";
             }
         }
-        
+
+
+        if (DinoStats.Instance.SetSkin == 0)
+        {
+            _aplGreenText.text = "Выбрано";
+        }
+        else if (DinoStats.Instance.SetSkin == 1)
+        {
+            _aplRedText.text = "Выбрано";
+        }
+        else if (DinoStats.Instance.SetSkin == 2)
+        {
+            _aplBlueText.text = "Выбрано";
+        }
+        else if (DinoStats.Instance.SetSkin == 3)
+        {
+            _aplYellowText.text = "Выбрано";
+        }
+        Debug.Log(DinoStats.Instance.SetSkin);
+
     }
 }
